b3fb87568239be0d6b24479cf8eb8dd7ae534c85
SaivNator <sivertsac@gmail.com>
2018-05-07 22:24:34 +0200

    added better .gitignore

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//main.cpp
+//Author: Sivert Andresen Cubedo
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+
+	std::cout << "Hello World!\n";
+
+#ifdef WINDOW
+	std::cout << "WINDOWS BINARY\n";
+#elif UNIX
+	std::cout << "UNIX BINARY\n";
+#endif
+
+	return 0;
+}
