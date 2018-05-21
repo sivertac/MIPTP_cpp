c961b5cb0c54726a758e0fe787fbada2e479daf1
SaivNator <sivertsac@gmail.com>
2018-05-21 23:22:49 +0200

    finally building deamons

--- src/MIP_deamon.cpp
+++ src/MIP_deamon.cpp
@@ -3,6 +3,7 @@
 
 //C++
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <vector>
 #include <tuple>
@@ -33,31 +34,86 @@ struct ARPPair
 Globals
 */
 std::vector<RawSock::MIPRawSock> raw_sock_vec;		//(must keep order intact so not to invalidate ARPPairs)
-std::vector<ARPPair> arp_pair_vec;
+std::vector<ARPPair> arp_pair_vec;					//(can't have duplicates)
+
+/*
+Print arp_pair_vec.
+Parameters:
+Return:
+	void
+Global:
+	arp_pair_vec
+*/
+void printARPTable()
+{
+	std::printf("%s: %4s:\n", "MIP", "MAC");
+	for (auto & p : arp_pair_vec) {
+		std::printf("%d: %4x:%x:%x:%x:%x:%x\n", (int)p.mip, p.mac[0], p.mac[1], p.mac[2], p.mac[3], p.mac[4], p.mac[5]);
+	}
+}
 
 /*
 Add pair to arp_pair_vec.
 Parameters:
+	sock		ref to sock that Node was discovered on
+	mip			mip
+	mac			mac
 Return:
+	void
+Global:
+	arp_pair_vec
 */
+void addARPPair(RawSock::MIPRawSock & sock, MIPAddress mip, MACAddress mac)
+{
+	ARPPair pair;
+	pair.mip = mip;
+	pair.mac = mac;
+	pair.sock = &sock;
+	arp_pair_vec.push_back(pair);
+}
+
+/*
+Send response frame to pair.
+Parameters:
+	pair		ref to pair
+Return:
+	void
+*/
+void sendResponseFrame(ARPPair & pair)
+{
+	static MIPFrame mip_frame;
+	mip_frame.setTRA(MIPFrame::ZERO);
+	mip_frame.setDest(pair.mip);
+	mip_frame.setSource(pair.sock->getMip());
+	mip_frame.setMsgSize(0);
+	mip_frame.setTTL(0xff);
+	static EthernetFrame eth_frame;
+	MACAddress dest = pair.mac;
+	MACAddress source = pair.sock->getMac();
+	eth_frame.setDest(dest);
+	eth_frame.setSource(source);
+	eth_frame.setProtocol(htons(RawSock::MIPRawSock::ETH_P_MIP));
+	eth_frame.setMsg(mip_frame.getData(), mip_frame.getSize());
+	pair.sock->sendEthernetFrame(eth_frame);
+}
 
 /*
 Send broadcast frame on sock.
 Parameters:
 	sock		ref to sock
 Return:
+	void
 */
 void sendBroadcastFrame(RawSock::MIPRawSock & sock)
 {
-	MIPFrame mip_frame;
+	static MIPFrame mip_frame;
 	mip_frame.setTRA(MIPFrame::A);
 	mip_frame.setDest(0xff);
 	mip_frame.setSource(sock.getMip());
 	mip_frame.setMsgSize(0);
 	mip_frame.setTTL(0xff);
-	
-	EthernetFrame eth_frame;
-	MACAddress dest{ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+	static EthernetFrame eth_frame;
+	static MACAddress dest{ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
 	MACAddress source = sock.getMac();
 	eth_frame.setDest(dest);
 	eth_frame.setSource(source);
@@ -75,10 +131,34 @@ Return:
 */
 void receiveRawSock(RawSock::MIPRawSock & sock)
 {
-	EthernetFrame frame;
-	sock.recvEthernetFrame(frame);
-	std::cout << frame.toString() << "\n";
-	std::cout << frame.getMsg() << "\n";
+	static EthernetFrame eth_frame;
+	sock.recvEthernetFrame(eth_frame);
+	MIPFrame mip_frame(eth_frame.getMsg(), eth_frame.getMsgSize());
+	int tra = mip_frame.getTRA();
+	MIPAddress mip = mip_frame.getSource();
+	std::vector<ARPPair>::iterator arp_it;
+	switch (tra)
+	{
+	case MIPFrame::A:
+		arp_it = std::find_if(arp_pair_vec.begin(), arp_pair_vec.end(), [&](ARPPair & p) { return p.mip == mip; });
+		if (arp_it == arp_pair_vec.end()) {
+			addARPPair(sock, mip_frame.getSource(), eth_frame.getSource());
+			sendResponseFrame(arp_pair_vec.back());
+		}
+		else {
+			sendResponseFrame((*arp_it));
+		}
+		break;
+	case MIPFrame::ZERO:
+		arp_it = std::find_if(arp_pair_vec.begin(), arp_pair_vec.end(), [&](ARPPair & p) { return p.mip == mip; });
+		if (arp_it == arp_pair_vec.end()) {
+			addARPPair(sock, mip_frame.getSource(), eth_frame.getSource());
+		}
+		break;
+	default:
+		throw std::runtime_error("Invalid TRA");
+		break;
+	}
 }
 
 /*
@@ -160,22 +240,9 @@ int main(int argc, char** argv)
 		epoll.addFriend<RawSock::MIPRawSock>(sock);
 	}
 
-	//test
+	//Send first broadcast frames
 	for (auto & s : raw_sock_vec) {
-		EthernetFrame frame;
-
-		MACAddress dest{ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
-		MACAddress source = s.getMac();
-		std::string msg("fuck you");
-
-		frame.setDest(dest);
-		frame.setSource(source);
-		frame.setProtocol(htons(RawSock::MIPRawSock::ETH_P_MIP));
-
-		auto p = msg.c_str();
-		frame.setMsg(p, msg.size());
-
-		s.sendEthernetFrame(frame);
+		sendBroadcastFrame(s);
 	}
 
 	while (epoll.wait() == EventPoll::Okay) {
